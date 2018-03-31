3b5895bd574f4a2071faeb794d6ef7402ab5b7b9
Unknown <vikram.cte@gmail.com>
2018-03-31 18:20:23 -0500

    Fixed Login

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
-
-    document.getElementById("logger_in_state").style.display = "block";
+    window.location="HomePage.html"
+    document.getElementById("logged_in_state").style.display = "block";
     document.getElementById("logged_out_state").style.display = "none";
   } else {
     // No user is signed in.
 
-    document.getElementById("logger_in_state").style.display = "block";
-    document.getElementById("logged_out_state").style.display = "none";
+    document.getElementById("logged_in_state").style.display = "none";
+    document.getElementById("logged_out_state").style.display = "block";
   }
 });
 
@@ -20,10 +21,9 @@ function login(){
 
 var username = document.getElementById("email_field").value;
 var pass = document.getElementById("password_field").value;
-
 window.alert(username + " " + pass );
 
-firebase.auth().createUserWithEmailAndPassword(email_field, password_field).catch(function(error) {
+firebase.auth().createUserWithEmailAndPassword(username, pass).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
@@ -32,6 +32,23 @@ firebase.auth().createUserWithEmailAndPassword(email_field, password_field).catc
   window.alert("Error: " + errorMessage);
 });
 
+var user = firebase.auth().currentUser;
+var Name = user.displayName;
+
+if(user != null){
+
+  var email = user.email;
+
+  document.getElementById("user").innerHTML = + Name;
+}
+
+}
 
 
+function logout(){
+  firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+}).catch(function(error) {
+  // An error happened.
+});
 }
