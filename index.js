50526b2ad725e4e536954ceb7d412f363eab0352
Unknown <vikram.cte@gmail.com>
2018-04-11 10:26:04 -0500

    Update v1.1

---
+++ index.js
@@ -0,0 +1,69 @@
+
+
+firebase.auth().onAuthStateChanged(function(user) {
+  var user = firebase.auth().currentUser;
+  if (user) {
+    // User is signed in.
+    document.getElementById("logged_in_state").style.display = "initial";
+    document.getElementById("logged_out_state").style.display = "none";
+  } else {
+    // No user is signed in.
+	document.getElementById("logged_in_state").style.display = "none";
+	document.getElementById("logged_out_state").style.display = "initial";
+  }
+});
+
+
+function login(){
+
+var username = document.getElementById("email_field").value;
+var pass = document.getElementById("password_field").value;
+
+
+
+window.alert(username + " " + pass );
+
+// firebase.auth().createUserWithEmailAndPassword(userN, userPass).catch(function(error) {
+//   // Handle Errors here.
+//   window.alert(userN + " " +userPass);
+//   var errorCode = error.code;
+//   var errorMessage = error.message;
+//   // ...
+// });
+
+firebase.auth().signInWithEmailAndPassword(username,pass).catch(function(error) {
+    // Handle Errors here.
+    
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    
+    if (errorCode == 'auth/wrong-password') {
+        alert('Wrong password.');
+    } else {
+        alert(errorMessage);
+    }
+    console.log(error);
+}); 
+
+//console.log("Fourth Test");
+
+if(user != null){
+    var user = firebase.auth().currentUser;
+    var Name = user.displayName;
+ 	  var email = user.email;
+  	document.getElementById("user").innerHTML = + Name;
+
+}
+
+}
+
+
+function logout(){
+
+  firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+}).catch(function(error) {
+  // An error happened.
+});
+}
+
--- js/index.js
+++ js/index.js
@@ -1,16 +1,15 @@
-
-
 firebase.auth().onAuthStateChanged(function(user) {
+  var user = firebase.auth().currentUser;
   if (user) {
     // User is signed in.
-    window.location="HomePage.html"
-    document.getElementById("logged_in_state").style.display = "block";
+    document.getElementById("logged_in_state").style.display = "initial";
     document.getElementById("logged_out_state").style.display = "none";
+    window.location.href = "HomePage.html";
+    
   } else {
     // No user is signed in.
-
-  document.getElementById("logged_in_state").style.display = "none";
-  document.getElementById("logged_out_state").style.display = "block";
+	document.getElementById("logged_in_state").style.display = "none";
+	document.getElementById("logged_out_state").style.display = "initial";
   }
 });
 
@@ -19,48 +18,24 @@ function login(){
 
 var username = document.getElementById("email_field").value;
 var pass = document.getElementById("password_field").value;
-window.alert(username + " " + pass );
 
 firebase.auth().signInWithEmailAndPassword(username, pass).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
+  // Handle Errors here.
+  
+  	var errorCode = error.code;
     var errorMessage = error.message;
     
-    if (errorCode === 'auth/wrong-password') {
+    if (errorCode == 'auth/wrong-password') {
         alert('Wrong password.');
     } else {
         alert(errorMessage);
     }
     console.log(error);
-}); 
-
-/* SIGN IN CHECK
-firebase.auth().createUserWithEmailAndPassword(username, pass).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
   // ...
-
-  window.alert("Error: " + errorMessage);
 });
-*/
 
-var user = firebase.auth().currentUser;
-var Name = user.displayName;
+window.alert(username + " " + pass );
 
-if(user != null){
- 	var email = user.email;
-  	document.getElementById("user").innerHTML = + Name;
-}
+
 
 }
-
-
-function logout(){
-  firebase.auth().signOut().then(function() {
-  // Sign-out successful.
-}).catch(function(error) {
-  // An error happened.
-});
-}
-
