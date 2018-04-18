49943cbdc0ca50352c0352d00a03ad3e6092b32c
Unknown <mswig2@yahoo.com>
2018-04-18 18:23:31 -0500

    Started work on Database
    Started work on database and assigng templates data

--- js/task.js
+++ js/task.js
@@ -1,3 +1,25 @@
+var task ={
+  name: taskName,
+  percentage: 0,
+  priority: 0,
+  assigned: worker,
+  id: 0,
+  date: 000000,
+  catagory: null;
+} 
+
+function createTask(name, percent, prio, assigned){
+  new task;
+  task.name = name;
+  task.percentage = precent;
+  task.priority = prio;
+  task.assigned = assigned;
+  date = Date.now();
+
+  id = 1;
+}
+
+
 function move() {
   var elem = document.getElementById("myBar");   
   var width = 20; //starting width
@@ -12,3 +34,6 @@ function move() {
     }
   }
 }
+
+
+
