d0daea09d222dad71774db3573b504d6f977cc15
Unknown <pmoore202@gmail.com>
2018-04-21 21:34:40 -0500

    Major Update on Index.html
    This update includes a lot of other minor bug fixes on files other than index.html.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 firebase.auth().onAuthStateChanged(function(user) {
   var user = firebase.auth().currentUser;
-  
   if (user) {
     // User is signed in.
     document.getElementById("logged_in_state").style.display = "initial";
@@ -36,10 +35,23 @@ firebase.auth().signInWithEmailAndPassword(username, pass).catch(function(error)
     console.log(error);
 });
 
-window.alert(username + " " + pass );
-
 }
 
+function signUp(){
+  var name, username, email, uid, password;
+  name = document.getElementById("SignUpName").value;
+  username = document.getElementById("SignUpUserName").value;
+  email = document.getElementById("SignUpEmail").value;
+  password = document.getElementById("SignUpPassword").value;
+
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorMessage);
+      console.log(error);
+  });
+  window.alert("You will be redirected to our log in page. Proceed to sign in to your new account from there.");
+}
 function logout(){
  
 	firebase.auth().signOut().then(function() {
